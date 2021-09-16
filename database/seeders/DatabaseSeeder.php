0301c78899dbb9eded93075fc3c878929b294995
Shane Poppleton <shane@bjja.com.au>
2021-09-16 22:10:29 +1000

    Install livewire

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Shane Poppleton',
+            'email' => 'shane@alphasg.com.au',
+            'password' => Hash::make('secret'),
+            'email_verified_at' => now()
+        ]);
+
+        User::factory(10)->create();
     }
 }
